573a45e7c703fd122563a89fa2a2183f7b5a7a50
kalabarigi <82603974+kalabarigi@users.noreply.github.com>
2025-01-23 18:35:24 +0000

    Update create-ec2.tf

--- create-ec2.tf
+++ create-ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1" # Change this to your desired region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 module "ec2_instance" {
